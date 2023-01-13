fe7791d349ccc7111d04eee662e3105bbbed8474
Renato Marsalla <56097330+renatomarsalla@users.noreply.github.com>
2023-01-13 17:17:02 -0300

    Delete apiFoodExplorer directory

--- apiFoodExplorer/src/configs/auth.js
+++
@@ -1,9 +0,0 @@
-const jwtToken = {
-  jwt: {
-    // secret: "default",
-    secret: process.env.AUTH_SECRET || "default",
-    expiresIn: "1d"
-  }
-}
-
-module.exports = { jwtToken }
--- apiFoodExplorer/src/database/knex/index.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const configKnex = require('../../../knexfile.js');
-
-const connectionKnex = knex(configKnex.development);
-
-module.exports = { connectionKnex };
